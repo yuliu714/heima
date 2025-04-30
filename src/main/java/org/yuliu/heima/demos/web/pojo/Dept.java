1b8bd095595e00bb097ad4d089817daa1162713d
yuliu <978869095@qq.com>
2025-04-30 15:20:32 +0800

    尝试git吧，网不好不推送

--- src/main/java/org/yuliu/heima/demos/web/pojo/Dept.java
+++ src/main/java/org/yuliu/heima/demos/web/pojo/Dept.java
@@ -1,4 +1,10 @@
 package org.yuliu.heima.demos.web.pojo;
 
+import java.time.LocalDateTime;
+
 public class Dept {
+    private int id;
+    private String name;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
---
+++ src/main/java/org/yuliu/heima/demos/web/pojo/Result.java
@@ -0,0 +1,32 @@
+package org.yuliu.heima.demos.web.pojo;
+
+public class Result {
+    private int code;//1成功 0失败
+    private String msg;
+    private Object data;//Object是所有java的基类
+
+    //成功响应
+    public static Result success() {
+        Result result = new Result();
+        result.code =1;
+        result.msg = "success";
+        return result;
+    }
+    //重载
+    public static Result success(Object data) {
+        Result result = new Result();
+        result.data=data;
+        result.code =1;
+        result.msg = "success";
+        return result;
+    }
+
+    //error
+    public static Result error(String msg) {
+        Result result = new Result();
+        result.msg=msg;
+        result.code =0;
+        return result;
+    }
+
+}
