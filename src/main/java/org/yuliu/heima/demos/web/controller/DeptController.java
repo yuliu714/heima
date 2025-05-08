a9cbaacb699c12788f3c5391acb95cc774be8725
yuliu <978869095@qq.com>
2025-05-08 08:33:21 +0800

    实现了增删改查，还有日志功能

--- src/main/java/org/yuliu/heima/demos/web/controller/DeptController.java
+++ src/main/java/org/yuliu/heima/demos/web/controller/DeptController.java
@@ -10,19 +10,20 @@ import org.yuliu.heima.demos.web.service.DeptService;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RequestMapping("/depts")
 @RestController
 public class DeptController {
 
     @Autowired
     private DeptService deptService;
 
-    @GetMapping("/depts")//限定访问方式维get
+    @GetMapping
     public Result list() {
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
 
-    @DeleteMapping("/depts")
+    @DeleteMapping
     public Result delete( Integer deptId) {
         System.out.println("删除的id为:"+deptId);
         //又是controller调用service
@@ -31,25 +32,23 @@ public class DeptController {
     }
 
     //ok这个新增的后端接口我来写，
-    @PostMapping("/depts")
-    public Result add(@RequestParam String name) {
-        System.out.println( name);
+    @PostMapping
+    public Result add(@RequestBody Dept dept) {
+        System.out.println(dept.getName());
         //又是controller调用service
-        Dept dept = new Dept();
-        dept.setName(name);
         dept.setCreateTime(LocalDateTime.now());
         dept.setUpdateTime(LocalDateTime.now());
         deptService.add(dept);
         return Result.success();
     }
     //这个也自己写
-    @GetMapping("/depts/{id}")
+    @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id){
         Dept dept = deptService.findById(id);
         return Result.success(dept);
     }
     //修改
-    @PutMapping("/depts")
+    @PutMapping
     public Result update(@RequestBody Dept dept){
         System.out.println("修改的部门为："+dept);
         deptService.update(dept);
