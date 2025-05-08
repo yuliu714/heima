b205d7dac5dfb94a077cd394c5777ff15c51dc7f
yuliu <978869095@qq.com>
2025-05-08 09:08:31 +0800

    v3基础班

--- src/main/java/org/yuliu/heima/demos/web/controller/DeptController.java
+++ src/main/java/org/yuliu/heima/demos/web/controller/DeptController.java
@@ -1,6 +1,7 @@
 package org.yuliu.heima.demos.web.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.yuliu.heima.demos.web.pojo.Dept;
@@ -10,6 +11,7 @@ import org.yuliu.heima.demos.web.service.DeptService;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
@@ -19,13 +21,14 @@ public class DeptController {
 
     @GetMapping
     public Result list() {
+        log.info("查询所有部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
 
     @DeleteMapping
     public Result delete( Integer deptId) {
-        System.out.println("删除的id为:"+deptId);
+        log.info("删除部门：{}", deptId);
         //又是controller调用service
         deptService.deleteById(deptId);
         return Result.success();
@@ -34,7 +37,7 @@ public class DeptController {
     //ok这个新增的后端接口我来写，
     @PostMapping
     public Result add(@RequestBody Dept dept) {
-        System.out.println(dept.getName());
+        log.info("新增部门：{}", dept);
         //又是controller调用service
         dept.setCreateTime(LocalDateTime.now());
         dept.setUpdateTime(LocalDateTime.now());
@@ -44,13 +47,14 @@ public class DeptController {
     //这个也自己写
     @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id){
+        log.info("根据id查询部门：{}", id);
         Dept dept = deptService.findById(id);
         return Result.success(dept);
     }
     //修改
     @PutMapping
     public Result update(@RequestBody Dept dept){
-        System.out.println("修改的部门为："+dept);
+        log.info("修改的部门为：{}", dept);
         deptService.update(dept);
         return Result.success();
     }
